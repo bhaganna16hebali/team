8e3233d0d5015f9a5c0b9a0362b4f47b9f984b5f
bhaganna16hebali <bhaganna7996@gmail.com>
2022-08-10 12:30:06 +0530

    commited

---
+++ Complaint_Reg/src/Demo.java
@@ -0,0 +1,17 @@
+
+public class Demo {
+	public static void main(String[] args)
+	{
+		int[] a=new int[4];
+		a[0]=10;
+		a[1]=20;
+		a[2]=25;
+		a[3]=30;
+		
+		for(int  i:a)
+		{
+			System.out.println(i);
+		}
+		
+	}
+}
